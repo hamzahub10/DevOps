292350a1f79b7e18a38b4e0c306e645240a27510
Fatma Alaya <alaya.fatma@esprit.tn>
2024-11-11 13:49:58 +0100

    add to repo

---
+++ src/test/java/tn/esprit/tpfoyer17/TpFoyer17ApplicationTests.java
@@ -0,0 +1,11 @@
+package tn.esprit.tpfoyer17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class TpFoyer17ApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
